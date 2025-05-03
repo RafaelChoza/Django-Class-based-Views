467c046be4e45d0dfa549200dc5ccffc692a798a
Rafael <166154431+RafaelChoza@users.noreply.github.com>
2025-05-03 11:52:53 -0600

    first commit

---
+++ src/products/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView, RedirectView
+
+from products.views import(
+    ProductListView, 
+    ProductDetailView, 
+    DigitalProductListView, 
+    ProductRedirectIdView, 
+    ProductRedirectView,
+    ProtectedProductDetailView,
+    ProtectedProductCreateView,
+    ProtectedProductUpdateView,
+    ProtectedProductDeleteView,
+    )
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("about-us/", RedirectView.as_view(url = "/products/about/")),
+    path("about/", TemplateView.as_view(template_name = "products/about.html")),
+    path("team/", TemplateView.as_view(template_name = "products/team.html")),
+    path("products/", ProductListView.as_view(template_name = "products/product_list.html")),
+    path("products/<int:pk>", ProductDetailView.as_view(template_name = "products/product_detail.html")),
+    path("digital-products/", DigitalProductListView.as_view(template_name = "products/product_list.html")),
+    path("products/<slug:slug>/", ProductDetailView.as_view()),
+    path("p/<int:pk>/", ProductRedirectIdView.as_view()),
+    path("p/<slug:slug>/", ProductRedirectView.as_view()),
+
+    #path("my-products/<slug:slug>/", ProtectedProductDetailView.as_view()),
+    path("my-products/create/", ProtectedProductCreateView.as_view()),
+    path("my-products/<slug:slug>/", ProtectedProductUpdateView.as_view()),
+    path("my-products/<slug:slug>/delete/", ProtectedProductDeleteView.as_view()),
+    path("my-products/", ProductListView.as_view(template_name = "products/product_list.html")),
+]
---
+++ src/products/views.py
@@ -0,0 +1,117 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.views.generic import (
+    View, 
+    TemplateView, 
+    RedirectView, 
+    ListView, 
+    DetailView, 
+    RedirectView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    )
+from django.shortcuts import render, get_object_or_404
+
+from .mixins import TemplateTitleMixin
+from .models import Product, DigitalProduct
+from .forms import ProductModelForm
+
+# def about_us_redirect_view(request):
+#     return HttpResponseRedirect("/about/")
+
+# def about_us_view(request):
+#     return render(request, "about.html", {})
+
+# class AboutUsView(View):
+#     def get(self, request):
+#         return render(request, "about.html", {})
+
+# class AboutUsView(TemplateView):
+#     template_name = "about.html"
+
+# class AboutUsView(TemplateView):
+#     template_name = "bout.html"
+
+# class AboutUsViewRedirectView(RedirectView):
+#     url = "/product/about/"
+
+class ProductListView(TemplateTitleMixin, ListView):
+    # app_label = "product"
+    # model = Product
+    # view_name = list
+    # template_name = <app_name>/<model>_<view_name>.html
+    # template_name = product/product_list.html
+    model = Product
+    title = "Productos Físicos"
+
+class ProductDetailView(DetailView):
+    # app_label = "product"
+    # model = Product
+    # view_name = detail
+    # template_name = <app_name>/<model>_<view_name>.html
+    # template_name = product/product_detail.html
+    model = Product
+
+class DigitalProductListView(TemplateTitleMixin, ListView):
+    model = Product
+    template_name = "products/product_list.html"
+    title = "Productos Digitales"
+
+class ProductRedirectIdView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        url_params = self.kwargs
+        pk = url_params.get("pk")
+        obj = get_object_or_404(Product, pk=pk)
+        slug = obj.slug
+        return f"/products/products/{slug}"
+        
+class ProductRedirectView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        url_params = self.kwargs
+        slug = url_params.get("slug")
+        return f"/products/products/{slug}"
+    
+class ProtectedProductDetailView(LoginRequiredMixin, DetailView):
+    model = Product
+
+class ProtectedProductCreateView(LoginRequiredMixin, CreateView):
+    form_class = ProductModelForm
+    template_name = "products/forms.html"
+
+    def form_valid(self, form):
+        form.instance.user = self. request.user
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+    
+class ProtectedProductUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = ProductModelForm
+    template_name = "products/product_detail.html"
+    
+    def get_queryset(self):
+        return Product.objects.filter(user=self.request.user)
+    
+    def get_success_url(self):
+        return self.object.get_edit_url()
+    
+class ProtectedProductDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = "products/forms_delete.html"
+
+    def get_queryset(self):
+        return Product.objects.filter(user=self.request.user)
+    
+    def get_success_url(self):
+        return "/products/products"
+    
+    class ProtectecProductListView(LoginRequiredMixin, ListView):
+        model = Product
+        template_name = "products/product_list.html"
+        
+        def get_queryset(self):
+            return Product.objects.filter(user=self.request.user)
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context["title"] = "Mis Productos"
+            return context
